204e45733b06212761e3d743393eaf5e2f70fd34
Anthony Minardo <minardar@rose-hulman.edu>
2013-03-16 15:06:33 -0400

    made test2 for GR

--- Lab4/src/PrimeFactorsTest.java
+++ Lab4/src/PrimeFactorsTest.java
@@ -12,4 +12,16 @@ public class PrimeFactorsTest {
 		assertEquals(new ArrayList<Integer>(), PrimeFactors.generateFactors(1));
 	}
 
+	@Test
+	public void testGeneratingFactors2() {
+		assertEquals(list(2), PrimeFactors.generateFactors(2));
+	}
+	
+	private ArrayList<Integer> list(int... ints){
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (int i : ints){
+			ret.add(i);
+		}
+		return ret;
+	}
 }
