5987c0450cbe44f19edb18461a9a16e7b4bdc687
Anthony Minardo <minardar@rose-hulman.edu>
2013-03-16 14:56:06 -0400

    made first test for GF

--- Lab4/src/PrimeFactorsTest.java
+++ Lab4/src/PrimeFactorsTest.java
@@ -1,13 +1,15 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
 public class PrimeFactorsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGeneratingFactors1() {
+		assertEquals(new ArrayList<Integer>(), PrimeFactors.generateFactors(1));
 	}
 
 }
