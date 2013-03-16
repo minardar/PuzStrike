5cde3e94798b304285d98f8412c566d0bcead60f
Anthony Minardo <minardar@rose-hulman.edu>
2013-03-16 15:09:20 -0400

    passed test2 for GR

--- Lab4/src/PrimeFactors.java
+++ Lab4/src/PrimeFactors.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 public class PrimeFactors {
 
 	public static ArrayList<Integer> generateFactors(int i){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+	
+		if (i > 1){
+			ret.add(i);
+		}
+		
+		return ret;
 	}
 }
