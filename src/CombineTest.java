a1fea1e195df68fc82ec5ff68d70783113eb7e12
harrissa <harrissa@rose-hulman.edu>
2013-04-11 21:18:44 -0400

    Combine is at 100% code coverage

---
+++ src/CombineTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class CombineTest {
+
+	@Test
+	public void testUseCombine() {
+		Game g = new Game(1);
+		Player combiner = g.getCurrentPlayer();
+		combiner.gemPile[0]=2;
+		ArrayList<String> b = new ArrayList<String>();
+		b.add("1 Gem");
+		b.add("1 Gem");
+		ArrayList<Object> j = new ArrayList<Object>();
+		j.add(0);
+		j.add(0);
+		Choice c = new Choice(g, "blah", b,j,1);
+		c.addChoice("1 Gem");
+		c.addChoice("1 Gem");
+		ArrayList<Choice> c3 = new ArrayList<Choice>();
+		c3.add(c);
+		Combine com = new Combine();
+		com.use(c3);
+		assertEquals(0, combiner.gemPile[0]);
+		assertEquals(1, combiner.gemPile[1]);
+	}
+	@Test
+	public void testCombineGetChoices(){
+		Game g = new Game(1);
+		Player combiner = g.getCurrentPlayer();
+		combiner.gemPile[0]=2;
+		Combine com = new Combine();
+		ArrayList<Choice> c = com.getChoice(g);
+		assertEquals("Choose the first gem to Combine!",c.get(0).getInstructions());
+	}
+	@Test
+	public void testNewCombine(){
+		Combine com = new Combine();
+		Card com2 = com.newCard();
+		assertEquals("Combine", com2.name);
+	}
+
+}
