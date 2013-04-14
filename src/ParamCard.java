d6f8c83367ae5a205f939f3c9667bd2087f0091b
kulczajr <kulczajr@rose-hulman.edu>
2013-04-14 11:14:23 -0400

    fixing

--- src/ParamCard.java
+++ src/ParamCard.java
@@ -3,37 +3,38 @@ import java.util.ArrayList;
 public class ParamCard extends Card {
 
 	private Player currentPlayer;
+	int blackTurns, purpleTurns, redTurns, brownTurns, blueTurns, draw, money;
 
 	public ParamCard(String name) {
+		this.cardColor.add(CardColor.BROWN);
+		this.cardType = cardType.PUZZLE;
+		this.defense = false;
+		this.value = 0;
+		this.name = "";
+		this.imagePath = name + ".png";
+		this.cost = 0;
 		
 		int blackTurns = 0, purpleTurns = 0, redTurns = 0, brownTurns = 0, blueTurns = 0, draw = 0, money = 0;
-		
-		if (name == "DrawThree") {
-			cost = 3;
-			draw += 3;
-		} else if (name == "OneTwoPunch") {
-			cost = 4;
-			blackTurns += 2;
-		} else if (name == "Roundhouse") {
-			cost = 6;
-			blackTurns += 1;
-			draw += 2;
-		} else if (name == "OneOfEach"){
-			cost = 5;
-			draw += 1;
-			blackTurns += 1;
-			money += 1;
-		}
-		
-		public Card DrawThree() {
-			return new DrawThreeCard();
-		}
+	}
+	
+	public void DrawThree() {
+		makeDrawThree();
+	}
+	
+	public void OneTwoPunch() {
+		makeOneTwoPunch();
 	}
 
 	@Override
 	public void use(ArrayList<Choice> choices) {
 		Player p = this.currentPlayer;
-
+		p.blackTurns += blackTurns;
+		p.purpleTurns += purpleTurns;
+		p.redTurns += redTurns;
+		p.brownTurns += brownTurns;
+		p.blueTurns += blueTurns;
+		p.money += money;
+		p.drawFromBag(draw);
 	}
 
 	@Override
@@ -50,33 +51,34 @@ public class ParamCard extends Card {
 		return new ParamCard(name);
 	}
 	
-	public class DrawThreeCard extends Card {
-		
-		public DrawThreeCard() {
-			this.cardColor.add(CardColor.BROWN);
-			this.cardType = cardType.PUZZLE;
-			this.defense = false;
-			this.value = 0;
-			this.name = "DrawThree";
-			this.imagePath = name + ".png";
-			this.cost = 4;
-		}
+	public void makeDrawThree() {
+		this.name = "DrawThree";
+		this.imagePath = name + ".png";
+		this.cost = 3;
+		draw += 3;
+	}
 
-		@Override
-		public ArrayList<Choice> getChoice(Game g) {
-			
-		}
-
-		@Override
-		public void use(ArrayList<Choice> choices) {
-			
-			
-		}
-
-		@Override
-		public Card newCard() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	private void makeOneTwoPunch() {
+		this.name = "DrawThree";
+		this.imagePath = name + ".png";
+		this.cost = 4;
+		blackTurns += 2;
+	}
+	
+	public void makeRoundHouse() {
+		this.name = "DrawThree";
+		this.imagePath = name + ".png";
+		this.cost = 6;
+		blackTurns += 1;
+		draw += 2;
+	}
+	
+	public void makeOneOfEach() {
+		this.name = "DrawThree";
+		this.imagePath = name + ".png";
+		this.cost = 5;
+		blackTurns += 1;
+		draw += 1;
+		money += 1;
 	}
 }
