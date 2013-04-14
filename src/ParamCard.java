9064244768beae207e9f6ed4b6a3157ee3e03f7e
kulczajr <kulczajr@rose-hulman.edu>
2013-04-14 10:44:40 -0400

    commiting to merge branch

--- src/ParamCard.java
+++ src/ParamCard.java
@@ -5,46 +5,40 @@ public class ParamCard extends Card {
 	private Player currentPlayer;
 
 	public ParamCard(String name) {
-		this.cardColor.add(CardColor.BROWN);
-		this.cardType = cardType.PUZZLE;
-		this.defense = false;
-		this.value = 0;
-		this.name = name;
-		this.imagePath = name + ".png";
-
+		
+		int blackTurns = 0, purpleTurns = 0, redTurns = 0, brownTurns = 0, blueTurns = 0, draw = 0, money = 0;
+		
 		if (name == "DrawThree") {
-			this.cost = 3;
+			cost = 3;
+			draw += 3;
 		} else if (name == "OneTwoPunch") {
-			this.cost = 4;
+			cost = 4;
+			blackTurns += 2;
 		} else if (name == "Roundhouse") {
-			this.cost = 4;
-		} else {
-			this.cost = 5;
+			cost = 6;
+			blackTurns += 1;
+			draw += 2;
+		} else if (name == "OneOfEach"){
+			cost = 5;
+			draw += 1;
+			blackTurns += 1;
+			money += 1;
+		}
+		
+		public Card DrawThree() {
+			return new DrawThreeCard();
 		}
 	}
 
 	@Override
 	public void use(ArrayList<Choice> choices) {
 		Player p = this.currentPlayer;
-		
-		if (name == "DrawThree") {
-			p.drawFromBag(3);
-		} else if (name == "OneTwoPunch") {
-			p.blackTurns += 2;
-		} else if (name == "Roundhouse") {
-			p.blackTurns += 1;
-			p.drawFromBag(2);
-		} else {
-			p.drawFromBag(1);
-			p.blackTurns += 1;
-			p.money += 1;
-		}
+
 	}
 
 	@Override
-	public ArrayList<Choice> getChoice(Game g) {
-		this.currentPlayer = g.getCurrentPlayer();
-		return new ArrayList<Choice>();
+	public ChoiceGroup getChoice(Game g) {
+		return new ChoiceGroup;
 	}
 
 	@Override
@@ -54,5 +48,35 @@ public class ParamCard extends Card {
 
 	public static Card newCard(String name) {
 		return new ParamCard(name);
+	}
+	
+	public class DrawThreeCard extends Card {
+		
+		public DrawThreeCard() {
+			this.cardColor.add(CardColor.BROWN);
+			this.cardType = cardType.PUZZLE;
+			this.defense = false;
+			this.value = 0;
+			this.name = "DrawThree";
+			this.imagePath = name + ".png";
+			this.cost = 4;
+		}
+
+		@Override
+		public ArrayList<Choice> getChoice(Game g) {
+			
+		}
+
+		@Override
+		public void use(ArrayList<Choice> choices) {
+			
+			
+		}
+
+		@Override
+		public Card newCard() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 	}
 }
