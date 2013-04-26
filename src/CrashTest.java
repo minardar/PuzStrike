2f2a8d09fa7fc244fe814e963c64a314661fbfd5
harrissa <harrissa@rose-hulman.edu>
2013-04-26 09:41:51 -0400

    Tested Crash

--- src/CrashTest.java
+++ src/CrashTest.java
@@ -32,6 +32,65 @@ public class CrashTest {
 		assertEquals(0, crasher.gemPile[0]);
 	}
 	@Test
+	public void TestReact(){
+		Game g = new Game(2);
+		Crash c = new Crash();
+		ArrayList<Choice> choose= new ArrayList<Choice>();
+		ArrayList<Object> gems = new ArrayList<Object>();
+		gems.add(1);
+		ArrayList<String> strgems = new ArrayList<String>();
+		strgems.add("1");
+		Choice choice1 = new Choice("pick gem", strgems, gems, 1);
+		choice1.addChoice("1");
+		choose.add(choice1);		
+		choose.add(choice1);
+		c.setChosenEffect(choose);
+		ArrayList<Object> o = g.getOpponentsObj();
+		Player p = (Player) o.get(0);
+		p.hand.add(new Gem(1));
+		ChoiceGroup c2 = c.getChoice(g);
+		c2.choices.get(0).getChoice().add(1);
+		c.react(c, p, c2.choices, g);
+		assertEquals(1, p.gemPile[0]);
+		
+	}
+	@Test
+	public void TestCanReactTo(){
+		Crash c1 = new Crash();
+		Crash c2 = new Crash();
+		
+		Combine c3 = new Combine();
+		assertTrue(c1.canReactTo(c2));
+		assertFalse(c1.canReactTo(c3));
+		
+	}
+	@Test
+	public void testPrepare(){
+		Crash c1 = new Crash();
+		ArrayList<String> strgems = new ArrayList<String>();
+		strgems.add("1");
+		strgems.add("1");
+		ArrayList<Object> gems = new ArrayList<Object>();
+		gems.add(1);
+		gems.add(1);
+		gems.add(1);
+		gems.add(1);
+		ArrayList<Choice> clist = new ArrayList<Choice>();
+		Choice choice = new Choice("pick gem", strgems, gems, 1);
+		ArrayList<Object> p = new ArrayList<Object>();
+		p.add(new Player());
+		p.add(new Player());
+		p.add(new Player());
+		
+		Choice choice2 = new Choice("Pic opponenet", strgems,p, 1);
+		choice2.addChoice("1");
+		choice.addChoice("1");
+		clist.add(choice2);
+		clist.add(choice);
+		c1.prepare(clist, new Game(3));
+		assertEquals(clist, c1.chosenEffect);
+	}
+	@Test
 	public void TestNewCrashWorks(){
 		Crash c = new Crash();
 		Card newC = c.newCard();
